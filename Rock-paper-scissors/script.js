71f682b4c5e25ab1588a73949dc60beacf490270
harsha12a <141937574+harsha12a@users.noreply.github.com>
2024-05-18 08:24:41 +0530

    add

--- Rock-paper-scissors/script.js
+++ Rock-paper-scissors/script.js
@@ -1,36 +1,43 @@
-const rock=document.getElementById("rock")
-const paper=document.getElementById("paper")
-const scissor=document.getElementById("scissor")
-const user=document.getElementById("user")
-const comp=document.getElementById("comp")
-const res=document.getElementById("res")
-const sub=document.getElementById("sub")
-rock.onclick=function(){
-    user.src=rock.src
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
+const user = document.getElementById("user");
+const comp = document.getElementById("comp");
+const res = document.getElementById("res");
+const sub = document.getElementById("sub");
+rock.onclick = function() {
+    user.src = rock.src;
+};
+paper.onclick = function() {
+    user.src = paper.src;
+};
+scissor.onclick = function() {
+    user.src = scissor.src;
+};
+function getrandom(arr, prevIndex) {
+    let i;
+    do {
+        i = Math.floor(Math.random() * arr.length);
+    } while (i === prevIndex);
+    return { element: arr[i], index: i };
 }
-paper.onclick=function(){
-    user.src=paper.src
-}
-scissor.onclick=function(){
-    user.src=scissor.src
-}
-function getrandom(arr){
-    var i=Math.floor(Math.random()*arr.length)
-    return arr[i]
-}
-var score=0
-sub.onclick=function(){
-    res.textContent=""
-    const arr=[rock,paper,scissor]
-    comp.src=getrandom(arr).src
-    if(comp.src===user.src)
-        res.textContent="Draw"
-    else if((comp.src===rock.src&&user.src===paper.src)||(comp.src===scissor.src&&user.src===rock.src)||(comp.src===paper.src&&user.src===scissor.src)){
-        score++
-        res.textContent="You won!!\nScore:"+score
+let score = 0;
+let prevIndex = -1;
+sub.onclick = function() {
+    res.textContent = "";
+    const arr = [rock, paper, scissor];
+    const { element: compChoice, index: newIndex } = getrandom(arr, prevIndex);
+    prevIndex = newIndex;
+    comp.src = compChoice.src;
+    if (comp.src === user.src) {
+        res.textContent = "Draw";
+    } else if ((comp.src === rock.src && user.src === paper.src) ||
+               (comp.src === scissor.src && user.src === rock.src) ||
+               (comp.src === paper.src && user.src === scissor.src)) {
+        score++;
+        res.textContent = "You won!!\nScore:" + score;
+    } else {
+        score = 0;
+        res.textContent = "Computer wins!\nScore:" + score;
     }
-    else{
-        score=0
-        res.textContent="Computer wins!\nScore:"+score
-    }
-}
+};
